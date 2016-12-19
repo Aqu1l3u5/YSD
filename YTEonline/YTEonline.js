01d5d3fb6d66aef92c9d34b7ad1fcb4f4fd1698c
Aqu1l3u5 <aqu1l3u5@gmail.com>
2016-12-19 13:13:01 +0100

    Create YTEonline.js

---
+++ YTEonline/YTEonline.js
@@ -0,0 +1,13 @@
+function ouvrirDoc () {
+    
+}
+
+function enregistrerDoc () {
+
+}
+
+var boutonOuvrirElt = document.getElementById("boutonOuvrir");
+boutonOuvrirElt.addEventListener("click", ouvrirDoc);
+
+var boutonEnregistrerElt = document.getElementById("boutonEnregistrer");
+boutonEnregistrerElt.addEventListener("click", enregistrerDoc);
