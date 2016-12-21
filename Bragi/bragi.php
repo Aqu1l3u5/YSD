65bedd034bb190082745eeba086a592f7b110849
Aqu1l3u5 <aqu1l3u5@gmail.com>
2016-12-21 14:40:11 +0100

    Add files via upload

--- Bragi/bragi.php
+++ Bragi/bragi.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	</head>
 
+	<body>
+		<form action="cible.php" method="post">
+			<h1>Bragi !!!</h1>
+			<input type="text" name="nomFichier" value="Nom de votre fichier" />
+			<input type="submit" value="Enregistrer" />
+			<textarea name="texte" rows="20" cols="170"></textarea> 
+		</form>
+	</body>
+</html>
