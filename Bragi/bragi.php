a04bcc78a40497bdf9b5c027bca759c3393cf1f0
Aqu1l3u5 <aqu1l3u5@gmail.com>
2016-12-22 16:28:39 +0100

    Add files via upload

--- Bragi/bragi.php
+++ Bragi/bragi.php
@@ -5,11 +5,49 @@
 	</head>
 
 	<body>
+		<h1>Bragi !!!</h1>
+		
+		<form method="post" action="bragi.php" enctype="multipart/form-data">
+     		        <input type="file" name="nomFichierOuvrir">
+     			<input type="submit" name="ouvrir" value="Ouvrir">
+		</form>
+				<?php
+					$texte = ""; 
+					if (!isset($_FILES["nomFichierOuvrir"]["name"]))
+					{
+
+					}
+					else
+					{
+						if ($_FILES["nomFichierOuvrir"]["error"] > 0) $erreur = "Erreur lors du transfert";
+						$extensionsValides = array( "txt" , "c" , "py" , "js" , "rb" );
+						$extensionsUpload = strtolower( substr( strrchr($_FILES["nomFichierOuvrir"]["name"], "."),1) );
+						if (! in_array($extensionsUpload,$extensionsValides)) $erreur = "Extension incorrecte";
+
+						$resultat = move_uploaded_file($_FILES["nomFichierOuvrir"]["tmp_name"], $_FILES["nomFichierOuvrir"]["name"]);
+						if ($resultat) $erreur = "Transfert réussi";
+
+						if (! $monFichierOuvrir = fopen($_FILES["nomFichierOuvrir"]["name"], "r"))
+						{
+							echo "echec de l'ouverture";
+							exit;
+						}
+						else
+						{
+							while (!feof($monFichierOuvrir))
+							{
+								$texte = $texte . fgets($monFichierOuvrir, 255);
+							}
+						}
+						fclose($monFichierOuvrir);
+						unlink($_FILES["nomFichierOuvrir"]["name"]);
+					}
+				?> 		
 		<form action="cibleBragi.php" method="post">
-			<h1>Bragi !!!</h1>
 			<input type="text" name="nomFichier" value="Nom de votre fichier" />
 			<input type="submit" value="Enregistrer" />
-			<textarea name="texte" rows="50" cols="170"></textarea> 
-		</form>
+			<textarea name="texte" rows="50" cols="170"><?php echo $texte; ?></textarea> 
+		</form>>
 	</body>
 </html>
+
--- Bragi/cibleBragi.php
+++ Bragi/cibleBragi.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-	</head>
-
-	<body>
-		<?php
-			$monFichier = fopen($_POST["nomFichier"], "a"); 
-			fputs($monFichier, $_POST["texte"]);
-			fclose($monFichier); 
-		?>
-	</body>
-</html>
-
-
+<?php
+	$nomFichier = $_POST["nomFichier"];
+	$monFichier = fopen($_POST["nomFichier"], "a"); 
+	fputs($monFichier, $_POST["texte"]);
+	fclose($monFichier);
+	header("Content-disposition: attachment; filename=$nomFichier");
+	header("Content-type: text/plain");
+	readfile($_POST["nomFichier"]);
+	unlink($_POST["nomFichier"]);  
+?>
 
