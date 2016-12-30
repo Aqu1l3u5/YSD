4b3a42bd9776174d79404a2dbd1aa5d26a1d8016
Aqu1l3u5 <aqu1l3u5@gmail.com>
2016-12-30 16:15:02 +0100

    Add files via upload

--- fr.pl
+++ fr.pl
@@ -35,6 +35,7 @@ while( defined(my $l = <$fh> ) ) { # lit le fichier source ligne par ligne
 	$l =~ s/EST DIFFERENT DE/!=/g; # opérateur différent
 	$l =~ s/EST INFERIEUR A/</g; # opérateur inférieur
 	$l =~ s/EST SUPERIEUR A/>/g; # opérateur supérieur
+	$l =~ s/EGAL/=/g; # égal, s'utilise lors d'un calcul
 	$l =~ s/PLUS/+/g; # plus, s'utilise lors d'un calcul (a = 2+3)
 	$l =~ s/MOINS/-/g; # moins, idem
 	$l =~ s/PUISSANCE/**/g; # puissance, idem
@@ -104,7 +105,7 @@ while( defined(my $l = <$fh> ) ) { # lit le fichier source ligne par ligne
 	$l =~ s/SEPARE LES LIGNES/splitlines/g; # retourne une liste des lignes d'une chaîne
 	$l =~ s/SEPARE/split/g; # transforme une chaîne de caractères en liste ("ma_chaîne".split("mon_séparateur"))
 	$l =~ s/MAJUSCULISE/upper/g; # met en majuscules une chaîne de caractères
-	$L =~ s/ET/and/g; # vérifie qu'une condition ET une autre soient vérifiées
+	$l =~ s/ET/and/g; # vérifie qu'une condition ET une autre soient vérifiées
 	$l =~ s/OU/or/g; # vérifie qu'une condition OU une autre soient vérifiées
 	$l =~ s/PAS/not/g; # vérifie qu'une condition n'est pas vérifiée
 	$l =~ s/ESSAYE/try/g; # essaye un bout de code
@@ -120,6 +121,123 @@ while( defined(my $l = <$fh> ) ) { # lit le fichier source ligne par ligne
 	$l =~ s/COMME/as/g; # permet de récupérer une valeur dun with (with ... as ... : ...)
 	$l =~ s/N'EST PAS/is not/g; # opérateur différent 
 	$l =~ s/EST/is/g; # opérateur égal
+
+	# la même chose mais en minuscule
+
+	$l =~ s/sinon si/elif/g; # condition elif
+	$l =~ s/sinon/else/g; # condition else
+	$l =~ s/si échoue/except/g; # except du bloc try
+	$l =~ s/si/if/g; # condition if
+	$l =~ s/alors/:/g; # le ":", then en basic
+	$l =~ s/tant que/while/g; # boucle while
+	$l =~ s/pour chaque/foreach/g; # boucle spécifique à un tableau (pour chaque élément du tableau faire ...)
+	$l =~ s/pour/for/g; # boucle for (pour chaque ... dans ... faire ...) 
+	$l =~ s/dans/in/g; # in, s'utilise avec for (for ... in ...)
+	$l =~ s/casse/break/g; # sert à sortir d'une boucle
+	$l =~ s/continue/continue/g; # fait reboucler la boucle
+	$l =~ s/tout est vrai/all/g; # savoir si chaque élément d'une liste est vrai (renvoie true si vrai) 
+	$l =~ s/est vrai parmi/any/g; # savoir si un élément d'une liste est vrai (renvoie true)
+	$l =~ s/vrai/true/g; # booléen vrai
+	$l =~ s/faux/false/g; # booléen faux
+	$l =~ s/écrit/print/g; # affiche du texte à l'écran
+	$l =~ s/demande une chaine/raw_input/g; # demande une saise à l'utilisateur qui sera convertie en chaîne de caractères
+	$l =~ s/demande/input/g; # demande une saisie à l'utilisateur
+	$l =~ s/nombre aléatoire/randint/g; # donne un nombre aléatoire, s'utilise avec le module random
+	$l =~ s/nombre/int/g; # type nombre
+	$l =~ s/décimale/float/g; # type décimale
+	$l =~ s/chaîne/str/g; # type chaîne de caractères
+	$l =~ s/égal/==/g; # opérateur égal
+	$l =~ s/est inférieur ou égal à/<=/g; # opérateur inférieur ou égal
+	$l =~ s/est supérieur ou égal à/>=/g; # opérateur supérieur ou égal 
+	$l =~ s/est différent de/!=/g; # opérateur différent
+	$l =~ s/est inférieur à/</g; # opérateur inférieur
+	$l =~ s/est supérieur à/>/g; # opérateur supérieur
+	$l =~ s/égal/=/g; # égal, s'utilise lors d'un calcul
+	$l =~ s/plus/+/g; # plus, s'utilise lors d'un calcul (a = 2+3)
+	$l =~ s/moins/-/g; # moins, idem
+	$l =~ s/puissance/**/g; # puissance, idem
+	$l =~ s/divisé avec entier par/\/\//g; # diviser en récupérant un entier, idem
+	$l =~ s/multiplié par/*/g; # multiplier, idem
+	$l =~ s/divisé par/\//g; # diviser, idem
+	$l =~ s/divisé avec reste par/%/g; # diviser en récupérant le reste, idem
+	$l =~ s/longueur/len/g; # retourne la longueur d'une chaîne de caractères ou d'une liste
+	$l =~ s/supprime/del/g; # supprime un élément d'une liste
+	$l =~ s/ajoute/append/g; # ajoute un élément à une liste
+	$l =~ s/trie liste/sorted/g; # trie une liste en créant une nouvelle liste 
+	$l =~ s/trie/sort/g; # trie une liste par ordre alphabétique ou numérique
+	$l =~ s/liste inversee/reversed/g; # inverse une liste en en créat une autre
+	$l =~ s/inverse/reverse/g; # inverse les éléments d'une liste
+	$l =~ s/indice/index/g; # trouve l'indice d'un élément d'une liste
+	$l =~ s/enlève/remove/g; # enlève un élément d'une liste
+	$l =~ s/gamme/range/g; # donne une liste préremplie d'éléments
+	$l =~ s/depuis/from/g; # sert à un importer un élément d'un module, s'utilise avec import (from ... import ...)
+	$l =~ s/importe/import/g; # sert à importer un élément d'un module, s'utilise avec from (idem)
+	$l =~ s/définition/def/g; # sert à définir une fonction (def ...( ... ) { ... })
+	$l =~ s/renvoie/return/g; # définit la valeur renvoyée par une fonction
+	$l =~ s/globale/global/g; # définit une variable globale au programme, réutilisable en dehors d'une fonction
+	$l =~ s/ouvre/open/g; # ouvre un fichier ( open("nom_du_fichier", "mode") )
+	$l =~ s/inscrit/write/g; # écrit une ligne à l'intérieur d'un fichier
+	$l =~ s/lit une ligne/readline/g; # lit une ligne d'un fichier
+	$l =~ s/lit/read/g; # lit tout un fichier
+	$l =~ s/ferme/close/g; # ferme un fichier
+	$l =~ s/type/type/g; # renvoie le type d'une variable
+	$l =~ s/mélange liste/zip/g; # regroupe sous la forme d'un tuple les items d'une liste
+	$l =~ s/additionne liste/sum/g; # additionne les valeurs d'un élément itérable 
+	$l =~ s/liste/list/g; # type liste
+	$l =~ s/compte les occurences i/count/g; # compte le nombre d'occurences d'une recherche dans une chaîne
+	$l =~ s/compte les occurences ii/count/g; # compte le nombre d'occurences d'une recherche dans une liste
+	$l =~ s/énumère/enumerate/g; # énumère les différents éléments d'une liste, est un itérateur 
+	$l =~ s/récupère/get/; # retourne la valeur d'une clé donnée, s'utilise sur un dictionnaire
+	$l =~ s/copie/copy/g; # copie un dictionnaire, dans un ordre différent
+	$l =~ s/absolue/abs/g; # retourne la valeur absolue d'un nombre
+	$l =~ s/binaire/bin/g; # convertit un nombre entier en chaîne de caractères binaires
+	$l =~ s/est appellable/callable/g; # détermine si on peu appeller un objet, retourne true si vérifié
+	$l =~ s/capitalise/capitalize/g; # formate une chaîne de caractère avec le format xxxxx
+	$l =~ s/choix/choice/g; # retourne aléatoirement une des valeurs d'une liste
+	$l =~ s/nom des structures/dir/g; # retourne les noms de la structure d'un objet
+	$l =~ s/se termine par/endswith/g; # teste si une chaîne de caractère se termine par la chaîne demandée, retourne true si vérifié
+	$l =~ s/calcule/eval/g; # calcule une chaîne de caractères (eval("2+2") retourne 4)
+	$l =~ s/trouve/find/g; # donne la place de la première occurence de la chaîne demandée
+	$l =~ s/aide/help/g; # retourne des informations sur un élément demandé
+	$l =~ s/hexadécimal/hex/g; # convertit un nombre en hexadécimal
+	$l =~ s/est alphanumérique/isalnum/g; # vérifie que tous les caractères d'une chaîne sont alphanumériques, renvoie true si vérifié
+	$l =~ s/est caractérielle/isalpha/g; # vérifie que tous les caractrèes d'une chaîne sont des lettres, idem
+	$l =~ s/est chiffrée/isdigit/g; # vérifie que tous les caractères d'une chaîne sont des chiffres, idem
+	$l =~ s/est minuscule/islower/g; # vérifie que tous les caractères d'une chaîne sont en minuscule, idem 
+	$l =~ s/est espacée/isspace/g; # vérifie que la chaîne contient au moins un espace
+	$l =~ s/est un titre/istitle/g; # vérifie que la chaîne à un format titre, c'est à dire xxxxx xx xxx xxxx 
+	$l =~ s/titre/title/g; # formate une chaîne au format titre, idem
+	$l =~ s/est majuscule/isupper/g; # vérifie que tous les caractères d'une chaîne sont en majuscules 
+	$l =~ s/joint/join/g; # transforme une liste en chaîne de caractères ("mon_séparateur".join(ma_liste))
+	$l =~ s/locales/locals/g; # retourne un dictionnaire avec les valeurs des variables en cours 
+	$l =~ s/minusculise/lower/g; # met en minuscules une chaîne de caractères
+	$l =~ s/faire/map/g; # execute une fonction sur chaque item d'une liste
+	$l =~ s/maximum/max/g; # retourne la valeur la plus élevée d'une liste 
+	$l =~ s/minimum/min/g; # retourne la valeur la plus basse d'une liste
+	$l =~ s/aléatoire/random/g; # retourne une valeur aléatoire
+	$l =~ s/remplace/replace/g; # remplace un segment d'une chaîne de caractères par une autre
+	$l =~ s/arrondi/round/g; # arrondi un nombre par le bas
+	$l =~ s/mélange/shuffle/g; # mélange aléatoirement une liste
+	$l =~ s/commence par/startswith/g; # vérifie qu'une chaîne de caractères commence par un préfixe indiqué
+	$l =~ s/sépare les lignes/splitlines/g; # retourne une liste des lignes d'une chaîne
+	$l =~ s/sépare/split/g; # transforme une chaîne de caractères en liste ("ma_chaîne".split("mon_séparateur"))
+	$l =~ s/majusculise/upper/g; # met en majuscules une chaîne de caractères
+	$l =~ s/et/and/g; # vérifie qu'une condition et une autre soient vérifiées
+	$l =~ s/ou/or/g; # vérifie qu'une condition ou une autre soient vérifiées
+	$l =~ s/pas/not/g; # vérifie qu'une condition n'est pas vérifiée
+	$l =~ s/essaye/try/g; # essaye un bout de code
+	$l =~ s/finalement/finally/g; # execute les instructions quelque soient les erreurs du bloc try
+	$l =~ s/classe/class/g; # regroupe les attributs et les fonctions qui définissent un objet
+	$l =~ s/elle-même/self/g; # indique qu'il s'agit d'un attribut d'un objet
+	$l =~ s/initialise/__init__/g; # initialise un objet
+	$l =~ s/nouveau/__new__/g; # construit un objet
+	$l =~ s/objet/object/g; # type objet
+	$l =~ s/dictionnaire/__dict__/g; # donne les valeurs de l'attribut d'instance
+	$l =~ s/donne/yield/g; # retourne une valeur sans la fin de la fonction
+	$l =~ s/avec/with/g; # execute du code avant et après un autre code
+	$l =~ s/comme/as/g; # permet de récupérer une valeur dun with (with ... as ... : ...)
+	$l =~ s/n'est pas/is not/g; # opérateur différent 
+	$l =~ s/est/is/g; # opérateur égal
 	print $fh2 $l; # écrit $l après l'avoir modifié
 }
 close($fh); # ferme le fichier source
