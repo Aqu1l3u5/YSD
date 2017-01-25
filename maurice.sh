8b77ac60ffa8e03820e3ed2b40c8f3407eed84c3
Aqu1l3u5 <aqu1l3u5@gmail.com>
2017-01-25 17:49:23 +0100

    Add files via upload

---
+++ maurice.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+clear
+
+echo "  JJJJJJJ   OOOO   YY  YY  EEEEEE  UU  UU  XX  XX"
+sleep 0.5
+echo "    JJJ    OOOOOO   YYYY   EE      UU  UU   XXXX "
+sleep 0.5
+echo "    JJJ    OO  OO    YY    EEEE    UU  UU    XX  "
+sleep 0.5
+echo "    JJJ    OOOOOO    YY    EE      UU  UU   XXXX "
+sleep 0.5
+echo "  JJJJ      OOOO     YY    EEEEEE   UUUU   XX  XX "
+sleep 0.5 
+echo "      "
+sleep 0.5
+echo "    AA    NN  NN  NN  NN    II   VV  VV  ''''' "
+sleep 0.5
+echo "   AAAA   NNNNNN  NNNNNN         VV  VV   '''' "
+echo "  AA  AA  NN NNN  NN NNN    II   VV  VV  ''' "
+sleep 0.5
+echo "  AAAAAA  NN  NN  NN  NN    II    VVVV  "
+sleep 0.5
+echo "  AA  AA  NN  NN  NN  NN    II     VV "
+sleep 0.5
+echo "      "
+sleep 0.5
+echo "  MM  MM    AA    UU  UU  RRRRR     II    CCCCC  EEEEEE  !!!!!!"
+sleep 0.5
+echo "  MMMMMM   AAAA   UU  UU  RR  RR         CCC     EE      !!!!!!"
+sleep 0.5
+echo "  MMMMMM  AA  AA  UU  UU  RRRRR     II   CC      EEEE     !!!!"
+sleep 0.5
+echo "  MM  MM  AAAAAA  UU  UU  RR RR     II   CCC     EE      "
+sleep 0.5
+echo "  MM  MM  AA  AA   UUUU   RR  RR    II    CCCCC  EEEEEE   !!!!"
+echo "                "
